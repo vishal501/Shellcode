1046153da4045c56d33cb65992afb3ea5c1ce16a
pkowledge <vs66429@gmail.com>
2022-01-12 20:01:40 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 import React, { useEffect, useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
-import Slider from './Slider';
+// import Slider from './Slider';
 // import $ from 'jquery';
 
 const Result = () => {
